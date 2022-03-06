33b9a0de11bea638728a9f2f7e88c6cd10a8d5d7
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-03-06 21:49:38 +0100

    fix: https heroku fix

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
+app.use(function (req, res, next){
+    if (req.headers['x-forwarded-proto'] === 'https') {
+      res.redirect('http://' + req.hostname + req.url);
+    } else {
+      next();
+    }
+  });
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
 app.listen(port);
